93f8b5e3814bd10fe5910c48a4efd75a4c7130a5
mickeyyo55b <mickeyyo55b@gmail.com>
2025-07-29 12:08:57 +0800

    07291208

--- src/main/java/tw/brad/spring2/controller/MemberController.java
+++ src/main/java/tw/brad/spring2/controller/MemberController.java
@@ -1,6 +1,10 @@
 package tw.brad.spring2.controller;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -8,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import tw.brad.spring2.entity.Member;
 import tw.brad.spring2.service.MemberService;
 
 @RestController
@@ -21,15 +26,28 @@ public class MemberController {
 		System.out.println("MemberController()");
 	}
 	
+	/*
+	 * response: true/false => Boolean
+	 */
 	@GetMapping("/exist")
-	public void checkAccount(@RequestParam String account) {
+	public ResponseEntity<Boolean> checkAccount(@RequestParam String account) {
 		boolean  isExist = memberService.checkAccount(account);
-		System.out.println(isExist);
+		return ResponseEntity.ok(isExist);
 	}
 	
+	/*
+	 * request: Member {}
+	 * response: {"message":"success/failure"} => Map<String,String>
+	 */
 	@PostMapping("/register")
-	public void register(@RequestBody String aaa) {
+	public ResponseEntity<Map<String,String>> register(@RequestBody Member member) {
 		
+		String result = memberService.register(member);
+		
+		Map<String,String> response = new HashMap<String, String>();
+		response.put("message", result);
+		
+		return ResponseEntity.ok(response);
 	}
 	
 }
--- src/main/java/tw/brad/spring2/service/MemberService.java
+++ src/main/java/tw/brad/spring2/service/MemberService.java
@@ -3,7 +3,9 @@ package tw.brad.spring2.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import tw.brad.spring2.entity.Member;
 import tw.brad.spring2.repository.MemberRepository;
+import tw.brad.spring2.utils.BCrypt;
 
 @Service
 public class MemberService {
@@ -13,9 +15,16 @@ public class MemberService {
 	public MemberService() {System.out.println("MemberService()");}
 	
 	public boolean checkAccount(String account) {
-		
-		
 		return memberRepository.existsByAccount(account);
+	}
+	
+	public String register(Member member) {
+		if (memberRepository.existsByAccount(member.getAccount())) {
+			return "Account 已使用";
+		}
+		member.setPasswd(BCrypt.hashpw(member.getPasswd(), BCrypt.gensalt()));
+		memberRepository.save(member);
+		return "註冊成功";
 	}
 	
 }
